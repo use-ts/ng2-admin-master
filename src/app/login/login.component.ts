541b06be39413d412ca7cc13570ee77904bf05a9
Chen, Jesse <Jesse.Chen@Honeywell.com>
2017-05-03 16:28:15 +0800

    登陆界面API接口POST请求调试通过

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { Http, Response, URLSearchParams, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+    public username:string;
+    public password:string;
+    public verifycode:string;
+
+    public url = "/fire-auth/login";
+    public headers = new Headers({'Content-Type': 'application/json;charset=utf-8'});
+
+    constructor(private router:Router, private http: Http) {
+
+    }
 
     ngOnInit(){
-
+      
     };
 
 
+    onSubmit() {
+
+      // this.router.navigate(['/main/1']);
+
+      // let params = new URLSearchParams();
+      // params.set('USERNMAE', this.username);
+      // params.set('PASSWORD', this.password);
+      // params.set('CHECKCODE', this.verifycode);
+      // console.log("params = " + params);
+
+
+      this.http.post(this.url,{"USERNAME":this.username,
+        "PASSWORD":this.password,
+        "CHECKCODE":this.verifycode},
+        {headers: this.headers}).subscribe(
+          res=>{
+            console.log(res);
+          },
+          err => console.log(err),
+          () => console.log('Register Complete'))
+
+
+
+
+    }
 
 }
