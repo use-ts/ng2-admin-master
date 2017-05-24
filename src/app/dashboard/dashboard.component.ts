393bf10218408b241b59bb358e6df482ef08c696
Chen, Jesse <Jesse.Chen@Honeywell.com>
2017-05-24 16:26:41 +0800

    getRealTimeFireList和getList的接口集成

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -22,15 +22,16 @@ export class DashboardComponent implements OnInit {
   public para = "";
   // @Input() dataURL:string="src/mock-data/postlist-mock.json";
   @Input() dataURL:string="src/mock-data/firelist-mock.json";
-  public postList:Array<any>;
+  public postList_dashboard:Array<any>;
+  public postList_detail:Array<any>;
   //相对地址
   //public url = "/fire-saas/DeviceEvent/getList";
   //绝对地址
   //火警监控接口
-  public url = "http://115.159.114.116:8082//fire-saas/DeviceEvent/getRealTimeFireList";
+  public url_dashboard = "http://115.159.114.116:8082//fire-saas/DeviceEvent/getRealTimeFireList";
   
   //消息详情接口
-  //public url = "http://115.159.114.116:8082/fire-saas/DeviceEvent/getList";
+  public url_detail = "http://115.159.114.116:8082/fire-saas/DeviceEvent/getList";
   public headers = new Headers({'Content-Type': 'application/json;charset=utf-8'});
 
   constructor (
@@ -60,360 +61,58 @@ export class DashboardComponent implements OnInit {
     //   );
 
 
-    //请求SaaS数据
-    this.http.post(this.url,{},
+    //请求SaaS数据，火警监控接口
+    this.http.post(this.url_dashboard,{},
         {headers: this.headers})
         .map(res => res.json())
         .subscribe(
           data=>{
             console.log("成功-->火警监控");
-            console.log("res = " + data);
             console.log("res.Success = "+ data.Success);
             console.log("res.Datas.tatal = "+ data.Datas.total);
             console.log("res.Datas.rows = "+ JSON.stringify(data.Datas.rows));
-            this.postList=data.Datas.rows;
-            //console.log("postList[1].fireDeviceEventList = "+ JSON.stringify(this.postList[1].fireDeviceEventList));
-
-
+            this.postList_dashboard=data.Datas.rows;
           },
           err => {
-            console.log("错误");
+            console.log("火警监控错误");
             console.log(err);  
           },
           () => {
-            console.log("完成");
+            console.log("火警监控完成");
             console.log('Login Complete');
           }
         );
   
 };
 
-    onClickCell() {
+  onClickCell() {
+        //请求SaaS数据，消息详情接口
+     this.http.post(this.url_detail,{},
+        {headers: this.headers})
+        .map(res => res.json())
+        .subscribe(
+          data=>{
+            console.log("成功-->消息详情");
+            console.log("res.Success = "+ data.Success);
+            console.log("res.Datas.tatal = "+ data.Datas.total);
+            console.log("res.Datas.rows = "+ JSON.stringify(data.Datas.rows));
+            this.postList_detail=data.Datas.rows;
+          },
+          err => {
+            console.log("消息详情错误");
+            console.log(err);  
+          },
+          () => {
+            console.log("消息详情完成");
+            console.log('Login Complete');
+          }
+        );
+
+
+
       console.log("-->onClickCell()");
-      //this.router.navigate(['/main/1']);
-
-      //this.parent.setActiveByPath("tables", this.parent.datatable);
-      //this.parent.changeChildNavStatis("tables", this.parent.datatable);
-
-      //this.router.navigate(['/detail']);
-      this.condition1 = false;  
-      this.condition2 = true;
-    }
-
-
-
-  public setPage(pageNo: number): void {
-    this.currentPage = pageNo;
-  }
-
-  public pageChanged(event: any): void {
-    console.log('Page changed to: ' + event.page);
-    console.log('Number items per page: ' + event.itemsPerPage);
-  }
-
-
-  settings = {
-    columns: {
-      property1: {
-        title: '控制器发生时间'
-      },
-      property2: {
-        title: '系统收到时间'
-      },
-      property3: {
-        title: '持续时长'
-      },
-      property4: {
-        title: '处理状态'
-      },
-      property5: {
-        title: '设备类型'
-      },
-      property6: {
-        title: '设备名称'
-      },
-      property7: {
-        title: '设备编号'
-      },
-      property8: {
-        title: '设备位置'
-      },
-      property9: {
-        title: '系统'
-      },
-      property10: {
-        title: '信息描述'
+        this.condition1 = false;  
+        this.condition2 = true;
       }
-    },
-    mode : "inline",
-    delete:{
-      confirmDelete : true
-    }
-  };
-
-  data = [
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    },
-    {
-      property1: "2017/1/21 13:56:23",
-      property2: "2017/1/21 13:57:23",
-      property3: "1分22秒",
-      property4: "未处理",
-      property5: "烟感探测器",
-      property6: "两层烟感",
-      property7: "L2.171",
-      property8: "3号楼402客房",
-      property9: "火灾自动报警系统",
-      property10: "连续报警"
-    }
-  ];
-
-  onDeleteConfirm(event): void {
-    console.log("delete function");
-    if (window.confirm('Are you sure you want to delete?')) {
-      event.confirm.resolve();
-    } else {
-      event.confirm.reject();
-    }/**/
-
-  }
 
 }
