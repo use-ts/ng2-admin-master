b52a91f30f55643c2b8068a07a64a3c35dae1a05
Chen, Jesse <Jesse.Chen@Honeywell.com>
2017-06-06 11:19:40 +0800

    Websocket两种返回类型，5种返回状态 N 未确认Y 前台WEB确认A 用户在设备上按下确认按键C 用户在设备上未按确认，直接按下复位按键 R 用户在设备上按确认后按下复位按键 

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -69,7 +69,8 @@ export class DashboardComponent implements OnInit{
 			createTime:'2017-01-01',
 			location:'南京霍尼韦尔',
 			deviceLabel:'1L2.345',
-			durationTime:'25s'
+			durationTime:'25s',
+			diviceCode:''
 		},
 		{
 			eventId:0,
@@ -80,7 +81,8 @@ export class DashboardComponent implements OnInit{
 			createTime:'2017-01-01',
 			location:'南京霍尼韦尔',
 			deviceLabel:'1L2.345',
-			durationTime:'25s'
+			durationTime:'25s',
+			diviceCode:''
 		},
 		{
 			eventId:0,
@@ -91,7 +93,8 @@ export class DashboardComponent implements OnInit{
 			createTime:'2017-01-01',
 			location:'南京霍尼韦尔',
 			deviceLabel:'1L2.345',
-			durationTime:'25s'
+			durationTime:'25s',
+			diviceCode:''
 		},
 		{
 			eventId:0,
@@ -102,7 +105,8 @@ export class DashboardComponent implements OnInit{
 			createTime:'2017-01-01',
 			location:'南京霍尼韦尔',
 			deviceLabel:'1L2.345',
-			durationTime:'25s'
+			durationTime:'25s',
+			diviceCode:''
 		},
 		{
 			eventId:0,
@@ -113,7 +117,8 @@ export class DashboardComponent implements OnInit{
 			createTime:'2017-01-01',
 			location:'南京霍尼韦尔',
 			deviceLabel:'1L2.345',
-			durationTime:'25s'
+			durationTime:'25s',
+			diviceCode:''
 		},
 		{
 			eventId:0,
@@ -124,7 +129,8 @@ export class DashboardComponent implements OnInit{
 			createTime:'2017-01-01',
 			location:'南京霍尼韦尔',
 			deviceLabel:'1L2.345',
-			durationTime:'25s'
+			durationTime:'25s',
+			diviceCode:''
 		},
 		{
 			eventId:0,
@@ -135,7 +141,8 @@ export class DashboardComponent implements OnInit{
 			createTime:'2017-01-01',
 			location:'南京霍尼韦尔',
 			deviceLabel:'1L2.345',
-			durationTime:'25s'
+			durationTime:'25s',
+			diviceCode:''
 		},
 		{
 			eventId:0,
@@ -146,7 +153,8 @@ export class DashboardComponent implements OnInit{
 			createTime:'2017-01-01',
 			location:'南京霍尼韦尔',
 			deviceLabel:'1L2.345',
-			durationTime:'25s'
+			durationTime:'25s',
+			diviceCode:'G64N1L2D4'
 		}
 	];
 
@@ -156,7 +164,11 @@ export class DashboardComponent implements OnInit{
 		this.condition2 = false;
 		this.parent.setActiveByPath (this.parent.dashboard, "");
 		this.para = this.parent.para;
+		//请求SaaS数据，火警监控接口
+		this.getFireData();
+	};
 
+	public getFireData():void {
 		//请求SaaS数据，火警监控接口
 		this.http.post (this.url_dashboard, {},
 			{headers:this.headers})
@@ -170,45 +182,68 @@ export class DashboardComponent implements OnInit{
 					this.postList_dashboard = data.Datas.rows;
 					this.cellNmuber = data.Datas.total;
 					this.eventNumber = data.Datas.rows.totalFireEvent;
-
-
 					//监听observable对象
-					this.postTableService.getPostTable ("")
+					let disposable = this.postTableService.getPostTable ("")
 						.subscribe (
 							res =>{
 								//console.log("监听observable对象:" +res);
-								console.log ("监听observable对象-->res.Datas.rows:" + JSON.stringify (JSON.parse (res).Datas.rows));
-								//新数组[0]添加到原来数组尾部
-								this.postList_dashboard.push (JSON.parse (res).Datas.rows[0]);
+								console.log ("监听observable对象-->res.Datas.rows:" + JSON.stringify (JSON.parse (res).Datas));
 
-								data = JSON.parse (res).Datas.rows[0];
-								let cellName:string = data['cellName'];
-								let buildName = data.buildWithFireEvent[0].fireDeviceEventList.buildName;
+								//下面的解析--Web State
+								let tempData = JSON.parse (res).Datas;
+								console.log ("监听observable对象-->res.Datas.rows:" + JSON.stringify (tempData));
 
-								//嵌套解析
-								for (let FireData of data.buildWithFireEvent){
-									for (let fireDeviceEventList of FireData.fireDeviceEventList){
-										for (let fireDeviceEventList02 of fireDeviceEventList.fireDeviceEventList){
-											let deviceId = fireDeviceEventList02.deviceId;
-											for (let eventItem of this.eventItems){
-												if (eventItem.deviceId===deviceId){
-													eventItem.createTime = fireDeviceEventList02['createTime'];
-													eventItem.location = cellName + buildName;
-													eventItem.deviceLabel = fireDeviceEventList02['deviceLabel'];
-													eventItem.durationTime = fireDeviceEventList02['duration'];
-													eventItem.eventId = fireDeviceEventList02['eventId'];
-													eventItem.eventTakeTime = fireDeviceEventList02['eventTakeTime'];
-													eventItem.confirmTime = fireDeviceEventList02['confirmTime'];
-													eventItem.confirmFlag = fireDeviceEventList02['confirmFlag'];
-													console.log ("监听observable对象-->修改后的的eventItem：" + JSON.stringify (eventItem));
-													break;
+								if (!('rows' in tempData)){
+									//修改后的数据
+									let deviceCode = tempData.deviceCode;
+									let confirmFlag = tempData.confirmFlag;
+									console.log('收到新的 ConfirmFlag: deviceCode : '+deviceCode+"  confirmFlag :"+confirmFlag);
+									//CR：用户按下复位键清除，重新刷新界面
+									if(confirmFlag==='C'||confirmFlag==='R') {
+										console.log ("用户按下复位键清除，重新刷新界面");
+										disposable.unsubscribe();
+										this.getFireData();
+									}
+									
+									for (let eventItem of this.eventItems){
+										if (eventItem.diviceCode===deviceCode){
+											eventItem.confirmFlag = confirmFlag;
+											console.log ("监听observable对象-->收到新的 ConfirmFlag：" + JSON.stringify (eventItem));
+										}
+									}
+								} else {
+									this.postList_dashboard.push (JSON.parse (res).Datas.rows[0]);
+									data = JSON.parse (res).Datas.rows[0];
+									let cellName:string = data['cellName'];
+									let buildName = data.buildWithFireEvent[0].fireDeviceEventList.buildName;
+
+									//嵌套解析
+									for (let FireData of data.buildWithFireEvent){
+										for (let fireDeviceEventList of FireData.fireDeviceEventList){
+											let deviceCode = fireDeviceEventList.deviceCode;
+											console.log('the device code :'+deviceCode);
+											for (let fireDeviceEventList02 of fireDeviceEventList.fireDeviceEventList){
+												let deviceId = fireDeviceEventList02.deviceId;
+												for (let eventItem of this.eventItems){
+													if (eventItem.deviceId===deviceId){
+														eventItem.createTime = fireDeviceEventList02['createTime'];
+														eventItem.location = cellName + buildName;
+														eventItem.deviceLabel = fireDeviceEventList02['deviceLabel'];
+														eventItem.durationTime = fireDeviceEventList02['duration'];
+														eventItem.eventId = fireDeviceEventList02['eventId'];
+														eventItem.eventTakeTime = fireDeviceEventList02['eventTakeTime'];
+														eventItem.confirmTime = fireDeviceEventList02['confirmTime'];
+														eventItem.confirmFlag = fireDeviceEventList02['confirmFlag'];
+														eventItem.diviceCode = deviceCode;
+														console.log ("监听observable对象-->修改后的的eventItem：" + JSON.stringify (eventItem));
+														break;
+													}
 												}
 											}
 										}
 									}
 								}
-
-
+								
 								// console.log ("监听observable对象-->单个火警：CellName:" + cellName + ' BuildName:' + buildName + ' deviceId:' + deviceId);
 								// // var eventItem = this.eventItems.find(x=>x.deviceId === deviceId);
 								// for (let eventItem of this.eventItems){
@@ -241,17 +276,13 @@ export class DashboardComponent implements OnInit{
 					console.log ("火警监控完成");
 					console.log ('Login Complete');
 				}
-			);
-
-
-	};
-
-	//视图模式切换
-	onClickView() {
+			);		
+	}
+	public onClickView():void {
 		console.log ("-->onClickView()");
 	}
 
-	onClickCell() {
+	public onClickCell():void {
 		//请求SaaS数据，消息详情接口
 		this.http.post (this.url_detail, {},
 			{headers:this.headers})
@@ -279,13 +310,13 @@ export class DashboardComponent implements OnInit{
 		this.condition2 = true;
 	}
 
-	onClickBack (){
+	public onClickBack():void {
 		console.log ("-->onClickBack()");
 		this.condition1 = true;
 		this.condition2 = false;
 	}
 
-	changeItemColor (flag:string){
+	public changeItemColor(flag:string):boolean {
 		return flag==='N';
 	}
 
